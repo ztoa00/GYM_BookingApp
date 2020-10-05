2a1f9c77065a177c51d851bfa89d7f1b6b4ec4d2
Rishi Nanthan <rishinanthan344@gmail.com>
2020-10-05 13:02:40 +0530

    7a reserved activity deletion

--- react_frontend/src/components/layout/MemberSchedules.js
+++ react_frontend/src/components/layout/MemberSchedules.js
@@ -1,60 +1,76 @@
-import React,{Fragment} from 'react';
+import React, { Fragment, useState } from 'react';
 import {Link} from 'react-router-dom';
 
 /* Import logo image */
 
 const MemberSchedules = () => {
+
+	const initial_state = [
+		{
+			name: "Rock Cycling",
+			time: 15,
+			day: "Martes",
+			clock: "11.00",
+		},
+		{
+			name: "Climbing",
+			time: 17,
+			day: "Jueves",
+			clock: "17.00",
+		},
+		{
+			name: "Running",
+			time: 19,
+			day: "Jueves",
+			clock: "08.00",
+		},
+	]
+
+	const [ schedules, set_schedules ] = useState(initial_state);
+
+	const remove_schedule = (schedule, index) => {
+
+		//  API call to remove schedule
+
+		let new_schedule = [];
+		schedules.forEach( (val, i) => {
+			if(index != i){
+				new_schedule.push(val);
+			}
+		});
+		set_schedules(new_schedule);
+	}
+
     return (
-    <Fragment>
-    	<div className="center-col-12">
-    		<div className="col-12 text-center horarios-lists">
-    			<i className="fas fa-calendar"></i>
-    			<h1 className="text-center">Horarios</h1>
-    		</div>
-    	</div>
-    	<div className="col-12 row horarios-lists" style={{marginTop: '0', paddingTop: '0'}}>
-    	<div className="schedule-card col-12 row" id="cycling">
-    		<div className="col-8">
-    			<h2>Rock Cycling</h2>
-   				<p><span id="time">15</span> Martes</p>
-   			</div>
-   			<div className="col-4">
-            	<i className="fa fa-minus-circle"></i>
-    			<p><i className="fa fa-clock"></i> 11.00</p>
-    		</div>
-    	</div>
-    	<div className="schedule-card col-12 row" id="climbing">
-    		<div className="col-8">
-    			<h2>Climbing</h2>
-   				<p><span id="time">17</span> Jueves</p>
-   			</div>
-   			<div className="col-4">
-            	<i className="fa fa-minus-circle"></i>
-    			<p><i className="fa fa-clock"></i> 17.00</p>
-    		</div>
-    	</div>
-   		<div className="schedule-card col-12 row" id="running">
-   			<div className="col-8">
-    			<h2>Running</h2>
-   				<p><span id="time">19</span> Jueves</p>
-    		</div>
-   			<div className="col-4">
-            	<i className="fa fa-minus-circle"></i>
-    			<p><i className="fa fa-clock"></i> 08.00</p>
-    		</div>
-    	</div>
-   		<div className="schedule-card col-12 row" id="running">
-   			<div className="col-8">
-    			<h2>Running</h2>
-   				<p><span id="time">19</span> Jueves</p>
-    		</div>
-   			<div className="col-4">
-            	<i className="fa fa-minus-circle"></i>
-    			<p><i className="fa fa-clock"></i> 08.00</p>
-    		</div>
-    	</div>
-    	</div>
-    </Fragment>
+		<Fragment>
+			<div className="center-col-12">
+				<div className="col-12 text-center horarios-lists">
+					<i className="fas fa-calendar"></i>
+					<h1 className="text-center">Horarios</h1>
+				</div>
+			</div>
+			<div className="col-12 row horarios-lists" style={{marginTop: '0', paddingTop: '0'}}>
+				{
+					schedules.map( (schedule, i) => {
+						return (
+							<div className="schedule-card col-12 row" id="running" key={ schedule.name + schedule.clock + schedule.day }>
+								<div className="col-8">
+									<h2>{ schedule.name }</h2>
+									<p><span id="time">{ schedule.time }</span> { schedule.day }</p>
+								</div>
+								<div className="col-4">
+									<i className="fa fa-minus-circle" onClick={ event => remove_schedule(schedule, i) }>d</i>
+									<p><i className="fa fa-clock"></i> { schedule.clock }</p>
+								</div>
+							</div>
+						);
+					})
+				}
+				{
+					schedules.length === 0 && <h3 className="text-center"> You have no active events... </h3>
+				}
+			</div>
+		</Fragment>
     );
 }
 
--- react_frontend/src/components/pages/Horarios.js
+++ react_frontend/src/components/pages/Horarios.js
@@ -14,7 +14,7 @@ class Horarios extends Component {
   		return (
 			<Fragment>
 				{/* Position: sticky couldn't work here, so I had to make the navbar class as fixed.
-  				* I inserted this div with a height of 66px here for the time being */}
+  				* I inserted this div with height of 66px here for the time being */}
 				<div style={{height: '66px'}}></div>
 				{(this.props.mode === 'member-mode') ? <MemberSchedules /> : <OwnerSchedules />}
 				{/* Filler for the fixed-positioned bottom-menu */}
